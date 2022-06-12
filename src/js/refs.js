bf2bb96852e8efccb1b6373e992b0cd14e9a3449
Roman <77742141+rumpear@users.noreply.github.com>
2022-06-12 14:54:31 +0300

    refs added

---
+++ src/js/refs.js
@@ -0,0 +1,3 @@
+export const form = document.querySelector('form');
+export const todoList = document.querySelector('.todo-list');
+export const taskCounter = document.querySelector('.task-counter');
